91c49cce7442c44b212add390497a3f362a159bf
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2023-05-13 17:45:31 +0200

    init

---
+++ src/main/java/it/vitalegi/jarg/JargApplication.java
@@ -0,0 +1,13 @@
+package it.vitalegi.jarg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JargApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JargApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/vitalegi/jarg/JargApplicationTests.java
@@ -0,0 +1,13 @@
+package it.vitalegi.jarg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JargApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
